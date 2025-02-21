a48ca86307950ab22cfe51de2ca071f6e8785909
mbahenryemeka <mbahenryemeka@gmail.com>
2025-02-21 05:48:50 -0500

    update package.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@
 
 
 import './App.css'
-import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+
 
 function App() {
   
 
   return (
     <>
-      <Home />
+      <Login />
     </>
   )
 }
