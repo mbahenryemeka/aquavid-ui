36f1404e4e365e90e04b77acb347c15485a767b2
mbahenryemeka <mbahenryemeka@gmail.com>
2025-05-10 09:40:58 -0400

    add route for the pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import SignUp from "./pages/signup/SignUp";
 import Login from "./pages/login/Login";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />}/>
       </Routes>
     </Router>
   );
