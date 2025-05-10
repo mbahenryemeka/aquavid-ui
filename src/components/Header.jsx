e47ff5dcee31c6d89713e6ff3661603b02c30121
mbahenryemeka <mbahenryemeka@gmail.com>
2025-05-10 11:11:35 -0400

    import aquavid logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Header.css";
 import MenuIcon from "../assets/menu-icon.png";
+import AquavidLogo from "../assets/aquavid-logo.png";
 
 function Header() {
   return (
     <header className='header'>
     <div>
       <img src={MenuIcon}/>
+      <img src={AquavidLogo}/>
     </div>
    </header>
   )
