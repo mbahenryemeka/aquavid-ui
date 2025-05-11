3d7d674ced3184d82d6dd571140fabd1b899c699
mbahenryemeka <mbahenryemeka@gmail.com>
2025-05-10 20:46:29 -0400

    import header icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,33 @@ import React from 'react'
 import "./Header.css";
 import MenuIcon from "../assets/menu-icon.png";
 import AquavidLogo from "../assets/aquavid-logo.png";
+import FormInput from './FormInput';
+import Upload from "../assets/upload.png";
+import Ellipes from "../assets/pic-icon.png";
+import Bell from "../assets/bell.png";
+import Red from "../assets/red-circle.png";
+import SmallEllipes from "../assets/small-ellipes.png"
+
 
 function Header() {
   return (
     <header className='header'>
     <div>
-      <img src={MenuIcon}/>
-      <img src={AquavidLogo}/>
+      <div className='header-logos-container'>
+        <div className='menu-aquavid-icon'>
+        <img src={MenuIcon} className='menu-icon'/>
+        <img src={AquavidLogo} className='aquavid-icon'/>
+        </div>        
+        <FormInput placeholder="search" className="form-input"/>
+        <div className='right-four-icons'>
+          <img src={SmallEllipes} />
+          <img src={Bell} />
+          <img src={Red} />
+          <img src={Ellipes}/>
+          <img src={Upload}/>
+        </div>
+      </div>
+     
     </div>
    </header>
   )
