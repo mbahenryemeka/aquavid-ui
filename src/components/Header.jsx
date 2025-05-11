4d2c4228fe652bed8864ecc4a70f292882f92b5e
mbahenryemeka <mbahenryemeka@gmail.com>
2025-05-10 21:26:25 -0400

    header arrangement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
+import React from "react";
 import "./Header.css";
 import MenuIcon from "../assets/menu-icon.png";
 import AquavidLogo from "../assets/aquavid-logo.png";
-import FormInput from './FormInput';
+import FormInput from "./FormInput";
 import Upload from "../assets/upload.png";
 import Ellipes from "../assets/pic-icon.png";
 import Bell from "../assets/bell.png";
 import Red from "../assets/red-circle.png";
-import SmallEllipes from "../assets/small-ellipes.png"
-
+import SmallEllipes from "../assets/small-ellipes.png";
+import Phantom from "../assets/phantom.png";
+import PhanStand from "../assets/phantom-stand.png";
 
 function Header() {
   return (
-    <header className='header'>
-    <div>
-      <div className='header-logos-container'>
-        <div className='menu-aquavid-icon'>
-        <img src={MenuIcon} className='menu-icon'/>
-        <img src={AquavidLogo} className='aquavid-icon'/>
-        </div>        
-        <FormInput placeholder="search" className="form-input"/>
-        <div className='right-four-icons'>
-          <img src={SmallEllipes} />
-          <img src={Bell} />
-          <img src={Red} />
-          <img src={Ellipes}/>
-          <img src={Upload}/>
+    <header className="header">
+      <div>
+        <div className="header-logos-container">
+          <div className="menu-aquavid-icon">
+            <img src={MenuIcon} className="menu-icon" />
+            <img src={AquavidLogo} className="aquavid-icon" />
+            <FormInput placeholder="search" className="form-input" />
+            <img src={SmallEllipes} />
+            <img src={Phantom} className="phantom" />
+            <img src={PhanStand} className="phanstand" />
+            <img src={Bell} />
+            <img src={Red} />
+            <img src={Ellipes} />
+            <img src={Upload} className="upload" />
+          </div>
         </div>
       </div>
-     
-    </div>
-   </header>
-  )
+    </header>
+  );
 }
 
-export default Header
+export default Header;
