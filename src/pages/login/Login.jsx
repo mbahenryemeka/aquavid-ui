8a6358bb23f00d316ed7ac4f08a2f26e46e65c44
mbahenryemeka <mbahenryemeka@gmail.com>
2025-06-25 03:34:11 -0400

    eslin

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,77 +5,74 @@ import LargeButton from "../../components/LargeButton";
 import SmallButton from "../../components/SmallButton";
 import { useNavigate } from "react-router-dom";
 
-
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    // Function to update username state
-    const handleUsernameChange = (event) => {
-      setUsername(event.target.value);
-    };
-  
-    // Function to update password state
-    const handlePasswordChange = (event) => {
-      setPassword(event.target.value);
-    };
+  // Function to update username state
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
-    const navigateTo = () => {
-      navigate("/signup")
-    }
+  // Function to update password state
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    function login(formData) {   
-      const username = formData.get("username");
-      const password = formData.get("password");
-      console.log(username);
-      console.log(password);      
-    }
+  const navigateTo = () => {
+    navigate("/signup");
+  };
+
+  function login(formData) {
+    const username = formData.get("username");
+    const password = formData.get("password");
+    console.log(username);
+    console.log(password);
+  }
 
   return (
     <div className="container">
       <div className="left-side">
         <p className="login-text">Login</p>
-         {/* Pass value and onChange to FormInput and  Wrap inputs and button inside a form*/}
-         <form action={login}>
-          <FormInput 
-            placeholder="Username" 
-            type="text" 
-            value={username} 
+        {/* Pass value and onChange to FormInput and  Wrap inputs and button inside a form*/}
+        <form action={login}>
+          <FormInput
+            placeholder="Username"
+            type="text"
+            value={username}
             onChange={handleUsernameChange}
-            name="username" 
+            name="username"
             required
           />
-          <FormInput 
-            placeholder="Password" 
-            type="password" 
-            value={password} 
+          <FormInput
+            placeholder="Password"
+            type="password"
+            value={password}
             onChange={handlePasswordChange}
             name="password"
             required
-          />       
+          />
           <p className="forget-password">forget password?</p>
-             {/* Trigger form submit */}
-          <LargeButton label="Login" type="submit"/>
+          {/* Trigger form submit */}
+          <LargeButton label="Login" type="submit" />
         </form>
         <div className="account-signup-container">
           <div className="have-account">
-            <p >Don't have an account?</p>
+            <p>Don't have an account?</p>
           </div>
           <div>
-            <SmallButton onClick={navigateTo} label="Signup"/>
-          </div>          
-        </div>       
+            <SmallButton onClick={navigateTo} label="Signup" />
+          </div>
+        </div>
       </div>
       <div className="right-side">
         <div className="center-logo">
           <img src="/images/logo.png" alt="Logo" />
-        </div>        
+        </div>
       </div>
     </div>
   );
 };
 
 export default Login;
-
-
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -6,41 +6,41 @@ import SmallButton from "../../components/SmallButton";
 import { useNavigate } from "react-router-dom";
 
 function SignUp() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const navigateTo = () => {
-    navigate("/login")
-  }
+    navigate("/login");
+  };
 
-   function signUp(formData) {  
-      const fullname= formData.get("fullname");
-      const email=formData.get("email");
-      const username = formData.get("username");
-      const password = formData.get("password");
-      console.log(username);
-      console.log(password);
-      console.log(fullname);
-      console.log(email);      
-    }
+  function signUp(formData) {
+    const fullname = formData.get("fullname");
+    const email = formData.get("email");
+    const username = formData.get("username");
+    const password = formData.get("password");
+    console.log(username);
+    console.log(password);
+    console.log(fullname);
+    console.log(email);
+  }
   return (
     <div className="container">
       <div className="left-side">
         <p className="signup-text">Sign Up</p>
         <form action={signUp}>
-          <FormInput placeholder="Full Name" type="text" name="fullname"/>
-          <FormInput placeholder="Username" type="text" name="username"/>
-          <FormInput placeholder="Email Address" type="email" name="email"/>
-          <FormInput placeholder="Password" type="password" name="password"/>
+          <FormInput placeholder="Full Name" type="text" name="fullname" />
+          <FormInput placeholder="Username" type="text" name="username" />
+          <FormInput placeholder="Email Address" type="email" name="email" />
+          <FormInput placeholder="Password" type="password" name="password" />
           <FormInput placeholder="Confirm Password" type="password" />
-          <LargeButton label="Sign up" type="submit"/>
+          <LargeButton label="Sign up" type="submit" />
         </form>
         <div className="mini-container">
           <div>
             <p className="have-account">Already have an account?</p>
           </div>
           <div>
-          <SmallButton onClick={navigateTo} label="login"/>
-          </div>          
+            <SmallButton onClick={navigateTo} label="login" />
+          </div>
         </div>
       </div>
       <div className="right-side">
